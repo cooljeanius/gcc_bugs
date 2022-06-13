a1c0ca1e91bec115536f4d76ab9f961eef75fa42
Eric Gallager <egall@gwmail.gwu.edu>
2022-06-12 21:02:16 -0400

    Create icculus_twitter_thread.c
    taken from testcases from @icculus on Twitter: https://twitter.com/icculus/status/1536138762975076352

---
+++ icculus_twitter_thread.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 0;
+    do printf("%d\n", x++); while (x < 10);
+    return 0;
+}
+
+const const const static const inline int xx;
+
+xxx();
+
+int xxxx(struct { int a; int b; } x) {
+    for (struct { int a; int b; } ; ; ) {
+        printf("this doesn't have a warning flag either, fwiw\n");
+    }
+    return 0;
+}
